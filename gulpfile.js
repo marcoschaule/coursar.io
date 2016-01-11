e71d0c66cc64020bd2fda5ef5e3f211888698876
Marc Stark <self@marcstark.com>
2016-01-11 13:46:37 +0100

    Add bumping tasks

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,10 @@ var sourcemaps    = require('gulp-sourcemaps');
 var domSrc        = require('gulp-dom-src'); // TODO: delete
 var download      = require('gulp-download');
 var gCallback     = require('gulp-callback'); // TODO: delete
+var git           = require('gulp-git');
+var bump          = require('gulp-bump');
+var filter        = require('gulp-filter');
+var tagVersion    = require('gulp-tag-version');
 var templateCache = require('gulp-angular-templatecache');
 var runSequence   = require('run-sequence');
 
@@ -429,6 +433,45 @@ gulp.task('run:dev', (callback) => {
  */
 gulp.task('default', []);
 
+// *****************************************************************************
+// Version bumping
+// *****************************************************************************
+
+/**
+ * Tasks to bump a version by patch, feature or release (semver).
+ */
+gulp.task('bump:patch',   function() { return bumpVersion('patch'); });
+gulp.task('bump:feature', function() { return bumpVersion('minor'); });
+gulp.task('bump:release', function() { return bumpVersion('major'); });
+
+/**
+ * Bumping version number and tagging the repository with it.
+ * Please read http://semver.org/
+ *
+ * You can use the commands
+ *
+ *     gulp patch     # makes v0.1.0 → v0.1.1
+ *     gulp feature   # makes v0.1.1 → v0.2.0
+ *     gulp release   # makes v0.2.1 → v1.0.0
+ *
+ * To bump the version numbers accordingly after you did a patch,
+ * introduced a feature or made a backwards-incompatible release.
+ */
+function bumpVersion(strImportance) {
+    // get all the files to bump version in 
+    return gulp.src(['./package.json'])
+        // bump the version number in those files 
+        .pipe(bump({ type: strImportance }))
+        // save it back to filesystem 
+        .pipe(gulp.dest('./'))
+        // commit the changed version number 
+        .pipe(git.commit('Bump package version'))
+        // read only one file to get the version number 
+        .pipe(filter('package.json'))
+        // **tag it in the repository** 
+        .pipe(tagVersion());
+}
+ 
 // ********************************************************************************
 
 })();
