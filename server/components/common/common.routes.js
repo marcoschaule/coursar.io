a97b5b715d00262a65dc0d7a4b3c8e4bb58708f0
Marc Stark <self@marcstark.com>
2016-03-11 17:42:55 +0100

    Reduce server routing

--- server/components/common/common.routes.js
+++ server/components/common/common.routes.js
@@ -12,21 +12,6 @@ var AuthCtrl = require('../auth/auth.controller.js');
 var _isInit          = false;
 var _app, _env, _strStaticFolder;
 
-/**
- * Array of all public routes. Every route must be 
- * registered here before it can be used.
- * 
- * @type {Array}
- */
-var _arrRoutesPublic = [ // TODO: remove
-    '/',
-    '/sign-in',
-    '/sign-up',
-    '/forgot-username',
-    '/forgot-password',
-    '/reset-password/:lowerCaseCharsAndNumbers',
-];
-
 // *****************************************************************************
 // Routes linking
 // *****************************************************************************
@@ -78,38 +63,6 @@ function setPrivateRoutes() {
     if (!_isInit) {
         throw new Error('Router must be initialized first!');
     }
-}
-
-// *****************************************************************************
-// Helper functions
-// *****************************************************************************
-
-/**
- * Helper function to get all public routes as a regular expression.
- * @private
- * 
- * @return {Regex}  regular expression of all public routes
- */
-function _getPublicRoutes() { // TODO: remove
-    var _arrRoutesPublicLocal = _arrRoutesPublic.map(str => escapeRegex(str));
-    var regexRoutesPublic     = new RegExp('^(' + _arrRoutesPublicLocal.join('|') + ')$');
-    return regexRoutesPublic;
-}
-
-// *****************************************************************************
-
-/**
- * Helper function to escape a string for a regular expression.
- * @private
- * 
- * @param  {String} strSource  string of the source
- * @return {String}            string of the result
- */
-function escapeRegex(strSource) { // TODO: remove
-    var strResult = strSource;
-    strResult = strResult.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-    strResult = strResult.replace(':lowerCaseCharsAndNumbers', '[0-9a-z\\-]+');
-    return strResult;
 }
 
 // *****************************************************************************
